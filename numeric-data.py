5619159ed0194ec4579896ee719f13a2918ea235
Martinha <45223140+jackdeep89@users.noreply.github.com>
2022-10-24 19:56:13 +0700

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,12 @@
+"""
+Your module description
+"""
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
